ca7d67534b4ec59ffe99d50bbf5d2024f49a3996
Tanimowo Emmanuel <oluwasegunprosperity@gmail.com>
2021-08-26 17:14:09 +0100

    add

--- pages_project/settings.py
+++ pages_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-u&yphkeva&ouwl_l%@^#oss+d!1*-bh_+^0k6br^^o%71os9_e
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['emma-boy.herokuapp.com/', '127.0.0.1', 'localhost']
 
 
 # Application definition
